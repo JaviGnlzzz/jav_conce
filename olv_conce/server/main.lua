efcc3f2a2a89d783adece8f1616277d51a53d967
Javier <98654716+JaviGnlzzz@users.noreply.github.com>
2023-08-04 13:17:22 +0200

    Actualizar main.lua
    Actualizado la db y el server side para hacerlo funcional con los garajes defaults de esx , arreglado también la posible vulnerabilidad de server side.

--- olv_conce/server/main.lua
+++ olv_conce/server/main.lua
@@ -11,11 +11,9 @@ end)
 ESX.RegisterServerCallback('jav_conce:buyVehicle', function(source, cb, model, properties, plate)
     local xPlayer = ESX.GetPlayerFromId(source)
 
-    MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE plate = @plate ', {
-        ['plate'] = plate
-    }, function(result)
+    MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE plate = ? ', {plate}, function(result)
         if not result[1] then
-            MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, properties) VALUES (?, ?, ?)', {xPlayer.getIdentifier(), plate, properties}, function(result) end)
+            MySQL.Async.execute('INSERT INTO owned_vehicles (owner, plate, vehicle) VALUES (?, ?, ?)', {xPlayer.getIdentifier(), plate, properties}, function(result) end)
         end
     end)
 end)
